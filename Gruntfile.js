e80aaf9bdb3bb86e3ebafb12ef2dc03a946fbd39
Steve Barnett <steve@naga.co.za>
2015-08-24 17:13:58 +0200

    Add Phantomas first pass

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,11 +9,35 @@ module.exports = function(grunt) {
         jshintrc: true
       },
       files: 'Gruntfile.js'
+    },
+    phantomas: {
+      dev: {
+        options : {
+          assertions : {
+            notFound: 0,
+            cssCount: 2,
+            jsCount: 2,
+            imageCount: 10,
+            webfontCount: 2,
+            cachingNotSpecified: 0,
+            nodesWithInlineCSS: 0,
+            imagesScaledDown: 0,
+            DOMidDuplicated: 0,
+            hiddenContentSize: 1024,
+            jQueryVersionsLoaded: 1,
+            redirects: 0,
+            assetsNotGzipped: 0
+          },
+          // indexPath  : './phantomas/',
+          url        : 'http://example.com/'
+        }
+      }
     }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-phantomas');
 
   // Default task.
   grunt.registerTask('default', ['jshint']);
